acb015e505de0aa1dc8054c83d506772c1c3e53f
macjabeth <macbethjonathan@gmail.com>
2019-10-02 23:25:59 -0400

    add display component tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+test('displays `Unlocked/Open` with `green-led` classes by default', () => {
+  const { getByText } = render(<Display />);
+  expect(getByText('Unlocked')).toHaveClass('green-led');
+  expect(getByText('Open')).toHaveClass('green-led');
+});
+
+test('displays `Locked/Closed` with `red-led` classes when props are passed', () => {
+  const { getByText } = render(<Display closed locked />);
+  expect(getByText('Closed')).toHaveClass('red-led');
+  expect(getByText('Locked')).toHaveClass('red-led');
+});
